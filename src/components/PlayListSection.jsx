f6125e20a1d916067ea681e8920642d0e3e440f2
Griffin Overmyer <griffinovermyer@gmail.com>
2023-11-13 15:27:24 -0500

    working playlist passed to desktop environment with commit

--- src/components/PlayListSection.jsx
+++ src/components/PlayListSection.jsx
@@ -1,16 +1,44 @@
-import { Box, Grid, Typography } from "@mui/material";
-import { grey } from "@mui/material/colors";
+import { useState } from "react";
+import { blue, grey } from "@mui/material/colors";
 import Track from "./Track";
+import { Grid, Box, Typography } from "@mui/material";
+import { TextField } from "@mui/material";
 
 const PlayListSection = ({ playList, removeSongFromPlayList }) => {
+  const [playListName, setPlayListName] = useState("");
+
   return (
     <Grid item sm={12} md={6}>
       <Box bgcolor={grey[100]} borderRadius={2} p={2} height="100%">
         <Typography variant="h3" component="h4" fontSize="1.8rem">
           Playlist
         </Typography>
+        {playList.length > 0 && (
+          <TextField
+            id="playlist-name"
+            name="playlistName"
+            value={playListName}
+            onChange={(e) => setPlayListName(e.target.value)}
+            variant="standard"
+            placeholder="playlist title..."
+            fullWidth //allows width to be 60%
+            //fix fontSize to work
+            sx={{
+              display: "block",
+              width: "60%",
+              marginInline: "auto",
+              marginBottom: "1rem",
+              fontSize: "36px",
+              "& ::placeholder": {
+                color: blue[600],
+                opacity: 90,
+                fontWeight: 700,
+              },
+            }}
+          />
+        )}
         <hr />
-        {playList &&
+        {playList.length > 0 &&
           playList.map((song) => (
             <Track
               key={song.id}
